6dddb5af39dab0c16fbd8f8239065fa29131a690
maximm <fannomas88@gmail.com>
2023-05-09 13:16:23 +0300

    Implemented a much smoother dash mechanic

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,24 +3,36 @@ extends CharacterBody2D
 #Physical constants
 const SPEED = 270.0
 const JUMP_VELOCITY = -400.0
-const DASH_FORCE = 30.0
-const STOMP_MODIFIER = 50
+const DASH_SPEED = 1100
+const DASH_LENGTH = .1
+const FALL_MODIFIER = 50
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var jump_counter = 0
+
+
+#Conditional variables
+var is_dashing = false
+var is_stomping = false
+
+#Accesing other nodes and scenes
 @onready var animation = get_node("AnimationPlayer")
+@onready var dash = $Dash
 
 
-	
 func _physics_process(delta):
 	# Add the gravity
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		if Input.is_action_pressed("crouch"):
-			velocity.y += STOMP_MODIFIER
+			velocity.y += FALL_MODIFIER
+			#if velocity.y > 1000 :
+			#	is_stomping = true
 		if velocity.y > 0:
 			animation.play("Fall")
+			
 	
 	#Reset jump counter
 	if is_on_floor():
@@ -38,19 +50,21 @@ func _physics_process(delta):
 		jump_counter += 1
 
 	
+	#Get player direction
 	var direction = Input.get_axis("move_left", "move_right")
 	
+	#Flip animation based on player's direction
 	if direction == -1:
 		get_node("AnimatedSprite2D").flip_h = true
 	elif direction == 1:
 		get_node("AnimatedSprite2D").flip_h = false
-		
-		
+	
+	#Handling movement
 	if direction:
-		velocity.x = direction * SPEED
 		if Input.is_action_just_pressed("dash"):
-			velocity.x += move_toward(velocity.x, direction * 2500.0, 2000.0)
-
+			dash.start_dash(DASH_LENGTH)
+			animation.play("Dash")
+		velocity.x = direction * DASH_SPEED if dash.is_dashing() else direction * SPEED
 		if velocity.y == 0:
 			animation.play("Run")
 	else:
